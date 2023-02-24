363acc1a2468b77b4043b7545bfbce3ce7c1abdf
ZubriQ <zubrich157@gmail.com>
2023-02-25 00:07:04 +0700

    Result may be null now in the service response class.

--- Olymp Project/Services/Registration/RegistrationService.cs
+++ Olymp Project/Services/Registration/RegistrationService.cs
@@ -12,28 +12,28 @@ namespace Olymp_Project.Services.Registration
             _db = db;
         }
 
-        public async Task<ServiceResponse<Account?>> RegisterAccountAsync(Account account)
+        public async Task<ServiceResponse<Account>> RegisterAccountAsync(Account account)
         {
             if (!AccountValidator.IsValid(account))
             {
-                return new ServiceResponse<Account?>(HttpStatusCode.BadRequest);
+                return new ServiceResponse<Account>(HttpStatusCode.BadRequest);
             }
 
             bool emailExists = _db.Accounts.Where(a => a.Email == account.Email).Any();
             if (emailExists)
             {
-                return new ServiceResponse<Account?>(HttpStatusCode.Conflict);
+                return new ServiceResponse<Account>(HttpStatusCode.Conflict);
             }
 
             try
             {
                 await _db.Accounts.AddAsync(account);
                 await _db.SaveChangesAsync();
-                return new ServiceResponse<Account?>(HttpStatusCode.Created, account);
+                return new ServiceResponse<Account>(HttpStatusCode.Created, account);
             }
             catch (Exception)
             {
-                return new ServiceResponse<Account?>(HttpStatusCode.InternalServerError);
+                return new ServiceResponse<Account>(HttpStatusCode.InternalServerError);
             }
         }
     }
--- Olymp Project/Services/ServiceResponse.cs
+++ Olymp Project/Services/ServiceResponse.cs
@@ -1,8 +1,12 @@
-﻿namespace Olymp_Project.Services
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Olymp_Project.Services
 {
     public class ServiceResponse<T>
     {
         public HttpStatusCode StatusCode { get; set; }
+
+        [MaybeNull]
         public T? Result { get; set; } = default(T?);
 
         public ServiceResponse(HttpStatusCode statusCode, T result)
