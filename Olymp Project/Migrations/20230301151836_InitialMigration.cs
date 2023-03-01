1a76504d2988338f175b7d18f408a38e22ee1c00
ZubriQ <zubrich157@gmail.com>
2023-03-01 22:38:48 +0700

    Add migrations

---
+++ Olymp Project/Migrations/20230301151836_InitialMigration.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Olymp_Project.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Account",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(320)", maxLength: 320, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Account", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kind",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kind", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Location",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Latitude = table.Column<double>(type: "float", nullable: false),
+                    Longitude = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Location", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Animal",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Weight = table.Column<float>(type: "real", nullable: false),
+                    Length = table.Column<float>(type: "real", nullable: false),
+                    Height = table.Column<float>(type: "real", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LifeStatus = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ChipperId = table.Column<int>(type: "int", nullable: false),
+                    ChippingDateTime = table.Column<DateTime>(type: "datetime", nullable: false),
+                    ChippingLocationId = table.Column<long>(type: "bigint", nullable: false),
+                    DeathDateTime = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animal", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Animal_Account",
+                        column: x => x.ChipperId,
+                        principalTable: "Account",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Animal_Location",
+                        column: x => x.ChippingLocationId,
+                        principalTable: "Location",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AnimalKind",
+                columns: table => new
+                {
+                    AnimalId = table.Column<long>(type: "bigint", nullable: false),
+                    KindId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AnimalType", x => new { x.AnimalId, x.KindId });
+                    table.ForeignKey(
+                        name: "FK_AnimalType_Animal",
+                        column: x => x.AnimalId,
+                        principalTable: "Animal",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_AnimalType_Type",
+                        column: x => x.KindId,
+                        principalTable: "Kind",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VisitedLocation",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LocationId = table.Column<long>(type: "bigint", nullable: false),
+                    AnimalId = table.Column<long>(type: "bigint", nullable: false),
+                    VisitDateTime = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VisitedLocation", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VisitedLocation_Animal",
+                        column: x => x.AnimalId,
+                        principalTable: "Animal",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_VisitedLocation_Location",
+                        column: x => x.LocationId,
+                        principalTable: "Location",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animal_ChipperId",
+                table: "Animal",
+                column: "ChipperId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animal_ChippingLocationId",
+                table: "Animal",
+                column: "ChippingLocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AnimalKind_KindId",
+                table: "AnimalKind",
+                column: "KindId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VisitedLocation_AnimalId",
+                table: "VisitedLocation",
+                column: "AnimalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VisitedLocation_LocationId",
+                table: "VisitedLocation",
+                column: "LocationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AnimalKind");
+
+            migrationBuilder.DropTable(
+                name: "VisitedLocation");
+
+            migrationBuilder.DropTable(
+                name: "Kind");
+
+            migrationBuilder.DropTable(
+                name: "Animal");
+
+            migrationBuilder.DropTable(
+                name: "Account");
+
+            migrationBuilder.DropTable(
+                name: "Location");
+        }
+    }
+}
